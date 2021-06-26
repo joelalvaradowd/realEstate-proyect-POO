2ccc6dc35a4a7e6b7eef22edccd7d4be2819719b
Joel Alvarado <joealalv@espol.edu.ec>
2021-06-26 15:03:48 -0500

    Se agregaron clase Usuario y sistema

---
+++ Bienes Raices/src/modelo/Usuario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author User
+ */
+public class Usuario {
+    private String password;
+    private String user;
+    private String cedula;
+    private String nombre;
+    private String correo;
+}
