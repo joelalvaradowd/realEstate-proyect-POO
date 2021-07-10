d63b20614eafabd1655b7f5e04b0328e39635479
Eliottsant <eliisant@espol.edu.ec>
2021-07-10 17:20:59 -0500

    Calculadora alemana

--- Bienes Raices/src/modelo/CalculadoraPrestamoFrances.java
+++ Bienes Raices/src/modelo/CalculadoraPrestamoFrances.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package modelo;
-import java.util.Scanner;
+
+import java.util.ArrayList;
+
 /**
  *
  * @author User
@@ -14,23 +16,24 @@ public class CalculadoraPrestamoFrances extends CalculadoraPrestamo{
         super(costo,tasaInteres,cuotas);
     }
     @Override
-    public double calculadoraPrestamo() {
-        Scanner sc=new Scanner(System.in);
+    public ArrayList calculadoraPrestamo() {
         double c=super.getCosto();
         double t=super.getTasaInteres();
         int cuota=super.getCuotas();
         int menosAnios=cuota;
         double totalPagar=c;
+        ArrayList<Double> cuotasMensuales=new ArrayList<>();
         
         while(menosAnios>0){
             double cuotaMonto=c*((t*0.01)/1-(1+Math.pow(t*0.01,(-cuota))));
             double interesMonto=c*(t*0.01);
             double reduccionCapital=cuotaMonto-interesMonto;
             double capitalAdeudado=c-reduccionCapital;
+            cuotasMensuales.add(reduccionCapital);
             c=capitalAdeudado;
             totalPagar+=interesMonto;
             menosAnios--;
         }
-        return totalPagar;
+        return cuotasMensuales;
     }
 }
